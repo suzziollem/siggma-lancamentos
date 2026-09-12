24e30207d48ec4246d4c42a24b3e4f7a664e872a
suzziollem <suzzinuvem@gmail.com>
2026-09-12 16:49:36 -0300

    Atualiza arquivos do aplicativo pela internet

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,16 @@
-const CACHE = "siggma-diario-v5";
+const CACHE = "siggma-diario-v6";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
-self.addEventListener("fetch", event => event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request))));
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
